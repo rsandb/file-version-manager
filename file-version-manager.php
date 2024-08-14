4494b7cc81cef50f4dfb168893d013b102f05440
Riley Sandborg <rileysandborg@gmail.com>
2024-08-13 20:55:13 -0500

    Update file-version-manager.php

--- file-version-manager.php
+++ file-version-manager.php
@@ -2,19 +2,19 @@
 /*
 Plugin Name: File Version Manager
 Description: Conveniently upload and update files site-wide.
-Version: 0.5.1
+Version: 0.6.0
 Author: Riley Sandborg
 Author URI: https://rileysandb.org/
 */
 
 namespace LVAI\FileVersionManager;
 
-if ( ! defined( 'ABSPATH' ) ) {
+if (!defined('ABSPATH')) {
 	exit;
 }
 
 // Define PLUGIN_URL constant
-define( 'LVAI\FileVersionManager\PLUGIN_URL', plugin_dir_url( __FILE__ ) );
+define('LVAI\FileVersionManager\PLUGIN_URL', plugin_dir_url(__FILE__));
 
 #todo: add autoloader
 // require_once __DIR__ . '/vendor/autoload.php';
@@ -33,12 +33,12 @@ require_once __DIR__ . '/classes/UpdateIDsPage.php';
 require_once __DIR__ . '/classes/UpdateIDs.php';
 // require_once __DIR__ . '/ajax-handlers.php';
 
-$file_manager = new FileManager( $GLOBALS['wpdb'] );
+$file_manager = new FileManager($GLOBALS['wpdb']);
 $settings_page = new SettingsPage();
-$shortcode = new Shortcode( $GLOBALS['wpdb'] );
-$admin_page = new AdminPage( $file_manager );
+$shortcode = new Shortcode($GLOBALS['wpdb']);
+$admin_page = new AdminPage($file_manager);
 $update_ids = new UpdateIDs();
-$update_ids_page = new UpdateIDsPage( $update_ids );
+$update_ids_page = new UpdateIDsPage($update_ids);
 
 $plugin = new Plugin(
 	$file_manager,
@@ -50,8 +50,8 @@ $plugin = new Plugin(
 
 $plugin->init();
 
-register_activation_hook( __FILE__, [ Activate::class, 'activate' ] );
-register_deactivation_hook( __FILE__, [ Deactivate::class, 'deactivate' ] );
+register_activation_hook(__FILE__, [Activate::class, 'activate']);
+register_deactivation_hook(__FILE__, [Deactivate::class, 'deactivate']);
 
 // add_action( 'wp_ajax_bulk_delete_files', function () {
 // 	$file_list_table = new FileListTable( new FileManager( $GLOBALS['wpdb'] ) );
