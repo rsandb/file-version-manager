3600aa3e5a42eaa3bd911d87035b3711ad6f308f
Riley Sandborg <rileysandborg@gmail.com>
2024-09-23 11:05:25 -0500

    add toggle template

--- classes/Shortcode.php
+++ classes/Shortcode.php
@@ -312,7 +312,7 @@ class Shortcode {
 
 		<script>
 			document.addEventListener("DOMContentLoaded", function () {
-				var toggles = document.querySelectorAll(".fvm-toggle-group > span");
+				var toggles = document.querySelectorAll(".fvm-toggle-category > span");
 				toggles.forEach(function (toggle) {
 					var content = toggle.nextElementSibling;
 					toggle.setAttribute("aria-expanded", "false");
@@ -327,7 +327,7 @@ class Shortcode {
 						content.style.display = isExpanded ? "none" : "block";
 
 						// Toggle the '+' to '-' and vice versa
-						toggle.textContent = toggle.textContent.replace(/^[+-]/, isExpanded ? '+' : '-');
+						// toggle.textContent = toggle.textContent.replace(/^[+-]/, isExpanded ? '+' : '-');
 					});
 				});
 			});
@@ -375,8 +375,11 @@ class Shortcode {
 		<?php foreach ( $categories as $category ) : ?>
 			<?php if ( $category->cat_parent_id == $parent_id ) : ?>
 				<?php $has_content = true; ?>
-				<li id="fvm-category-<?php echo $category->id; ?>" class="fvm-toggle-group">
-					<span>+ <?php echo esc_html( $category->cat_name ); ?></span>
+				<li id="fvm-category-<?php echo $category->id; ?>" class="fvm-toggle-category" data-level="<?php echo $level; ?>">
+					<span class="fvm-toggle-category-title">
+						+
+						<?php echo esc_html( $category->cat_name ); ?>
+					</span>
 					<ul class="fvm-toggle-content" style="display: none;">
 						<?php
 						// Recursively render subcategories
@@ -387,7 +390,7 @@ class Shortcode {
 							return $item->id == $category->id && $item->file_id;
 						} );
 						foreach ( $category_files as $file ) {
-							echo $this->render_file_item( $file );
+							echo $this->render_file_item( $file, $level + 1 );
 						}
 						?>
 					</ul>
@@ -398,7 +401,7 @@ class Shortcode {
 		<?php if ( $level == 0 ) : ?>
 			<?php foreach ( $direct_files as $file ) : ?>
 				<?php $has_content = true; ?>
-				<?php echo $this->render_file_item( $file ); ?>
+				<?php echo $this->render_file_item( $file, $level ); ?>
 			<?php endforeach; ?>
 		<?php endif; ?>
 
@@ -407,17 +410,22 @@ class Shortcode {
 		return $has_content ? $output : '';
 	}
 
-	private function render_file_item( $file ) {
+	private function render_file_item( $file, $level ) {
 		ob_start();
 		?>
-		<li id="fvm-file-<?php echo $file->id; ?>" class="fvm-toggle-file">
-			<a href="<?php echo esc_url( $file->file_url ); ?>" target="_blank" rel="noopener noreferrer">
-				<?php echo esc_html( ! empty( $file->file_display_name ) ? $file->file_display_name : $file->file_name ); ?>
-			</a>
-			<?php if ( $file->file_type ) : ?>
-				<span class="fvm-item-size uppercase"><?php echo esc_html( $file->file_type ); ?></span>
-			<?php endif; ?>
-			<span class="fvm-item-size">(<?php echo esc_html( size_format( $file->file_size ) ); ?>)</span>
+		<li id="fvm-file-<?php echo $file->id; ?>" class="fvm-toggle-file" data-level="<?php echo $level; ?>">
+			<span>
+				<a href="<?php echo esc_url( $file->file_url ); ?>" title="<?php echo esc_attr( $file->file_display_name ); ?>"
+					target="_blank" rel="noopener noreferrer">
+					<?php echo esc_html( ! empty( $file->file_display_name ) ? $file->file_display_name : $file->file_name ); ?>
+				</a>
+				<span class="fvm-file-item-meta">
+					<?php if ( $file->file_type ) : ?>
+						<span><?php echo esc_html( $file->file_type ); ?></span>
+					<?php endif; ?>
+					<span>(<?php echo esc_html( size_format( $file->file_size ) ); ?>)</span>
+				</span>
+			</span>
 		</li>
 		<?php
 		return ob_get_clean();
